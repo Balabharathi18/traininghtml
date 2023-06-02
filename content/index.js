3b9962398dfab72b22432b822e302919d307af3f
Balabharathi18 <56287701+Balabharathi18@users.noreply.github.com>
2023-06-02 17:31:42 +0530

    Delete content directory

--- content/index.js
+++
@@ -1,9 +0,0 @@
-function submitpage(){
-    location.href="/content/submit.html"
-}
-function back(){
-    location.href="/index.html"
-}
-// document.getElementById("login").onclick=function(){
-//     location.href="subbmit.html"
-// }
