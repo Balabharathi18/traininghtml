ddbd62f1e65c360ada87c222e82c78066630fbc4
Balabharathi18 <56287701+Balabharathi18@users.noreply.github.com>
2023-06-02 14:51:13 +0530

    Add files via upload

---
+++ content/index.js
@@ -0,0 +1,9 @@
+function submitpage(){
+    location.href="/content/submit.html"
+}
+function back(){
+    location.href="/index.html"
+}
+// document.getElementById("login").onclick=function(){
+//     location.href="subbmit.html"
+// }
