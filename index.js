b72034ceefb809a4af502e5ba6689f81712a66b6
Balabharathi18 <56287701+Balabharathi18@users.noreply.github.com>
2023-06-02 13:28:38 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function submitpage(){
+    location.href="/submit.html"
+}
+function back(){
+    location.href="/index.html"
+}
+// document.getElementById("login").onclick=function(){
+//     location.href="subbmit.html"
+// }
